69df274a2774bd68f78c6713ac51d782da7658af
Cameron Troy <cameron@Camerons-MacBook-Pro.local>
2020-10-13 19:22:27 -0400

    initial commit of fresh project

---
+++ ser423/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ser423
+//
+//  Created by Cameron Troy on 10/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
