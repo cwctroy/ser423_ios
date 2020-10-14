e5e5d35e669b29249bf0762233a09675996ffc1c
Cameron Troy <camtroy93@gmail.com>
2020-10-13 20:24:20 -0400

    Add copyright to files

--- ser423/ViewController.swift
+++ ser423/ViewController.swift
@@ -1,10 +1,17 @@
-//
-//  ViewController.swift
-//  ser423
-//
-//  Created by Cameron Troy on 10/13/20.
-//
-
+ /* Copyright 2020
+  * Cameron W.C. Troy,
+  *
+  * This software is the intellectual property of the author, and can not be distributed, used, copied, or
+  * reproduced, in whole or in part, for any purpose, commercial or otherwise. The author grants the ASU
+  * Software Engineering program the right to copy, execute, and evaluate this work for the purpose of
+  * determining performance of the author in coursework, and for Software Engineering program evaluation,
+  * so long as this copyright and right-to-use statement is kept in-tact in such use.
+  * All other uses are prohibited and reserved to the author
+  *
+  * @author Cameron Troy cwtroy@asu.edu
+  * @version October 2020
+  */
+ 
 import UIKit
 
 class ViewController: UIViewController {
